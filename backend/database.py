56c0d2dcd0516cefd3909b999bebc3aa13f22303
Astra <markshelkonogov003@gmail.com>
2026-05-22 09:35:58 +0500

    add psycopg2 for postgresql

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db").replace("postgresql://", "postgresql+psycopg2://")
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
