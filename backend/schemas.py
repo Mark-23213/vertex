502fb3b50f35a0c3e9533e69396f12d6c090a61f
Mark-23213 <240411@turan-edu.kz>
2026-05-22 08:49:43 +0500

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,7 +3,7 @@ from typing import Optional
 from datetime import datetime
 
 class UserRegister(BaseModel):
-    username: str
+    name: str
     email: str
     password: str
 
@@ -13,7 +13,7 @@ class UserLogin(BaseModel):
 
 class UserResponse(BaseModel):
     id: int
-    username: str
+    name: str
     email: str
     created_at: datetime
     
